8456e8956321812e5a390f490dfd07eda46087df
João Ribeiro <jlribeiroaugusto@gmail.com>
2017-11-27 16:33:23 -0200

    nova versão

--- main.js
+++ main.js
@@ -1,40 +1,121 @@
-const Ajax = function(obj){
+/*
+* @Author: João Luís Ribeiro <joaoluis@ibaro.com.br>
+* @version: 2.0.0
+* @description: Ajaxabstract é uma bibliteca que usa XMLHttpRequest como abstração para facilitar o dia a dia do desenvolvedor web
+*/
+const Ajax = (function () {
+  const methods = ['get', 'post', 'put', 'delete']
+  const scope = {}
 
-	
-	// manipulating the object
-	this.obj = obj || {} || new Object();
-	
-	// Send the files from server if existes file
-	this.obj.data = function(data){
-		let sendDatas = new FormData(this.data);
-		return sendDatas;
-	}
-	
-	// Initialize Ajax Object
-	const request = new XMLHttpRequest();
-	request.open(this.obj.method, this.obj.url, true);
+  /**
+   * helperHttp é uma função privada para auxilio dos metodos post, put, get e delete
+   * @param  {[string]}   	method recebe um string indicando o metodo a ser usado
+   * @param  {[variable]}   uri recebe outro parametro
+   * @param  {Function} 	cb callback da resposta retornada pelo metodos
+   * @param  {[object]}   	data recebe um objeto por padrão é nulo
+   * @return {object}       retorna um objeto como resposta no callback
+   */
+  const helperHttp = (method, uri, cb, data = null) => {
+  	const xhr = new window.XMLHttpRequest()
 
-	// Action of request and response with load and error
-	request.addEventListener('load', this.obj.ok);
-	request.addEventListener('error', this.obj.error)
+    xhr.open(method, uri, true)
 
-	// Send of request's
-	request.send(this.obj.data);
+    if (scope.headers && typeof data !== 'object') {
+      xhr.setRequestHeader(scope.headers.contentType, scope.headers.application)
+    }
 
-}
+    xhr.onprogress = (data) => {
+    	scope.progress = data
+    }
 
-Ajax({
-	method: 'GET',
-	url: 'https://viacep.com.br/ws/01001000/json/',
-	ok: function(data){
-		if(data.target.status === 200){
-			const resp = JSON.parse(data.target.responseText);
-			return console.log(resp);
-		}
-	},
+    xhr.onerror = () => {
+      console.clear()
+      cb(null, {message: `Erro ao estabelecer conexão com ${uri}`, status: xhr.status})
+    }
 
-	error: function(){
-		console.clear();
-		return console.error("erro :", "ocorreu algum erro na requisição");
-	}
-});
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+      	let mimiType = xhr.getResponseHeader('Content-Type')
+      	let parser = null
+
+    	switch (mimiType) {
+    		case 'application/json':
+    			parser = JSON.parse(xhr.responseText)
+    			break
+    		case 'text/html':
+    			const parserDom = new DOMParser()
+        		parser = parserDom.parseFromString(xhr.responseText, 'text/html')
+        		break
+        	case 'application/xml':
+        		const parserXML = new DOMParser()
+        		parser = parserXML.parseFromString(xhr.responseText, 'application/xml')
+        		break
+        	default:
+        		parser = false
+    	}
+
+        cb({
+          data: xhr.responseText,
+          parsed: parser,
+          status: xhr.status,
+          statusText: xhr.statusText,
+          time: scope.progress,
+          mimitype: mimiType},
+        null)
+      } else {
+        cb(null, {message: xhr.responseText, status: xhr.status, time: scope.progress})
+      }
+    }
+
+    xhr.send(data)
+  }
+
+  /**
+   * configuração para envio de chave (opcional) metodo encadeado
+   * @param  {object} options.target recebe o formulario e atribui a scopo publico
+   * @param  {object} options.key    chave de acesso ou de atenticação
+   * @return {function}     retorna a propria função
+   */
+  scope.settings = ({target = 0, key = null}) => {
+    const form = document.forms[target] ? document.forms[target] : document.querySelector(target)
+
+    if (key) form.insertAdjacentHTML('beforeend', `<input type="hidden" value="${key}" name="key">`)
+    scope.target = form
+    return this
+  }
+
+  /**
+   * Configuração do cabeçalho da requesição
+   * @param  {string} contentType recebe otipo de conteudo
+   * @param  {string} application recebe otipo da aplicação
+   * @return {function}  retorna a propria função
+   */
+  scope.setHeader = (contentType, application) => {
+    scope.headers = {}
+    scope.headers['contentType'] = contentType
+    scope.headers['application'] = application
+    return this
+  }
+
+  /**
+   * Metodo de requesição post, put, get e delete
+   * @param  {array} method itera sobre o array de metodos
+   * @return {function}  retorna a propria função
+   */
+  methods.forEach(method => {
+    scope[method] = (uri, body = null, cb) => {
+      const data = (body instanceof FormData) ? body : JSON.stringify(body)
+
+      if (typeof body === 'function') {
+      	cb = body
+      }
+
+      helperHttp(method, uri, cb, data)
+      return this
+    }
+  })
+
+  return scope
+})()
+
+export default Ajax
